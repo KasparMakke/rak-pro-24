b4ed2ec1717a01d0ad896165224a382f424fe93e
KasparMakke <kasparmakke@gmail.com>
2024-09-02 17:32:34 +0300

    feat: Finished lesson #1 task #7
    - Renamed lessons to tasks
    - Closes #1

--- 24-09-02/FirstLesson.js
+++ 24-09-02/FirstLesson.js
@@ -1,4 +1,4 @@
-// Lesson #1
+// Task #1
 const numbers = [1, 3, 4, 8];
 
 function findNumberIndex(_array, _number){
@@ -9,7 +9,7 @@ function findNumberIndex(_array, _number){
 console.log(findNumberIndex(numbers, 1));
 
 
-// Lesson #2
+// Task #2
 function addNumbers(num1, num2){
     return num1 + num2;
 }
@@ -17,7 +17,7 @@ function addNumbers(num1, num2){
 console.log(addNumbers(3, 3));
 
 
-// Lesson #3
+// Task #3
 const addNumbersArrow = (num1, num2) => {
     return num1 + num2;
 }
@@ -25,13 +25,13 @@ const addNumbersArrow = (num1, num2) => {
 console.log(addNumbersArrow(26,4));
 
 
-// Lesson #4
+// Task #4
 const addNumbersArrowSmall = (num1, num2) => num1 + num2;
 
 console.log(addNumbersArrowSmall(6,4));
 
 
-// Lesson #5
+// Task #5
 function addNumbersNested(num1){
     return function (num2){
         return num1 + num2;
@@ -41,7 +41,16 @@ function addNumbersNested(num1){
 console.log(addNumbersNested(2)(3));
 
 
-// Lesson #6
+// Task #6
 const addNumbersNested2 = (num1) => (num2) => num1 + num2;
 
 console.log(addNumbersNested2(2)(4));
+
+
+// Task #7
+const greet = (_name = "Loser") => "Hello, " + _name;
+
+console.log(greet())
+console.log(greet("Gangster"))
+
+
